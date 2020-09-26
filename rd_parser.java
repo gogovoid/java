ff4609c6eb641030ab19980e3e8a23a0af4d6f77
root <root@void.localdomain>
2020-09-26 23:51:28 +0900

    java files

---
+++ rd_parser.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class rd_parser {
+	static char LA = 0;
+	static InputStreamReader isr;
+
+	static char nexttoken() throws IOException {
+		return (char) isr.read();
+	}
+	static void match(char token) throws IOException{
+		if(token == '\r'){
+			return ;
+		}
+		if(LA == token){
+			LA = nexttoken();
+			if(LA == '\r'){
+				LA = nexttoken();
+			}
+
+		}else{
+			error(token);
+		}
+	}
+	static void error(char c){
+		System.out.println("Syntax Error : '" + c + "'\n");
+		System.exit(-1);
+	}
+	static void K() throws IOException {
+		if((int)LA == -1)
+			return;
+		else{
+			S(); match('\n'); K();
+		}
+	}
+	static void S() throws IOException {
+		if(LA == '('){
+			match('('); S(); match(')'); S();
+		} else {
+			;
+		}
+	}
+	public static void main(String[] args) throws IOException{
+		isr = new InputStreamReader(System.in);
+		LA = nexttoken();
+		K();
+	}
+}
