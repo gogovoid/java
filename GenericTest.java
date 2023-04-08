35f9d58a2c06a5a34c528e8aa86344586040281c
gogovoid <71092342+gogovoid@users.noreply.github.com>
2023-04-09 02:22:57 +0900

    Revert "remove comments"

--- GenericTest.java
+++ GenericTest.java
@@ -12,9 +12,9 @@ public class GenericTest {
 	public static void main(String[] args) {
 		Map<String, String> subjects = new Hashtable<String, String>();
 		
-		subjects.put("pl", "ê¹€ì¸í•˜");
-		subjects.put("oop2", "ì´ì¸í•˜");
-		subjects.put("statistics", "ìµœì¸í•˜");
+		subjects.put("pl", "±èÀÎÇÏ");
+		subjects.put("oop2", "ÀÌÀÎÇÏ");
+		subjects.put("statistics", "ÃÖÀÎÇÏ");
 		
 		List<String> keys = new ArrayList<String>(subjects.keySet());
 		Collections.sort(keys);
@@ -22,7 +22,59 @@ public class GenericTest {
 		for(String key : keys) {
 			System.out.println(key);
 			System.out.println(subjects.get(key));
-		}	
+		}
+		
+//		TreeSet<String> scores = new TreeSet<String>();
+//		
+//		scores.add(new String("¹Ú"));
+//		scores.add("±è");
+//		scores.add("ÃÖ");
+//		scores.add("ÀÌ");
+//		scores.add("È«");
+//		scores.add("°¡");
+//		
+//		for(String score : scores) {
+//			System.out.print(score + " ");					
+//		}
+//		System.out.println();
+//		NavigableSet<String> descendingSet = scores.descendingSet();	
+//		for(String score : descendingSet) {
+//			System.out.print(score + " ");			
+//		}
+		
+		
+//		TreeSet<Integer> scores = new TreeSet<Integer>();
+//		
+//		scores.add(new Integer(66));
+//		scores.add(22);
+//		scores.add(0);
+//		scores.add(33);
+//		scores.add(88);
+//		scores.add(11);
+//		
+//		for(Integer score : scores) {
+//			System.out.print(score + " ");					
+//		}
+//		System.out.println();
+//		NavigableSet<Integer> descendingSet = scores.descendingSet();	
+//		for(Integer score : descendingSet) {
+//			System.out.print(score + " ");			
+//		}
+		
+//		List list = new ArrayList();
+//		list.add("hello");
+//		list.add("¾È³ç");
+//		list.add(1043215678);
+//		String s = (String)list.get(2);
+//		System.out.println(s);
+		
+		
+//		List<String> list = new ArrayList<String>();
+//		list.add("hello");
+//		list.add("¾È³ç");
+//		//list.add(1043215678);
+//		String s = list.get(0);   // no cast
+//		System.out.println(s);	 	
 	}
 
 }
